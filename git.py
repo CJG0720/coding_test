afa4bcee91ac4bbdda8a64c0ab2412f5511e9825
CJG0720 <chlworhks11@naver.com>
2022-01-13 22:46:40 +0900

    commit

--- git.py
+++ git.py
@@ -1,6 +1,10 @@
 '''
-소스제어 - 변경 내용 스테이징(+) - 커밋(v)
-git branch -> main
-git pull origin main --allow-unrelated-histories
-git push -u origin main
+1. 변경 내용 스테이징(+)
+git add {file name}
+
+2. 커밋(v)
+git commit -m 'commit'
+
+3. 
+git push
 '''
